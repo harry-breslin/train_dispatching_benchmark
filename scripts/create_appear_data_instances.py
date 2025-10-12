0ee01972737f72af7cb3db206f9f0d9d7f16601a
Harry Breslin <hbre0004@student.monash.edu>
2025-10-13 09:27:11 +1100

    Add create_appear_data_instances.py script

---
+++ scripts/create_appear_data_instances.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+import glob
+import csv
+
+input_folder = "data/cp2025"
+data_dir = "appear-cp2025"
+output_folder = f"data/{data_dir}"
+generator_model = "models/appear-instance-generator.mzn"
+instances_csv = "data/instances_sat_appear.csv"
+
+os.makedirs(output_folder, exist_ok=True)
+
+
+def has_origin_train(dzn_path):
+    with open(dzn_path, "r") as f:
+        return any("t_type" in line and "origin" in line for line in f)
+
+
+# generate new dzn files
+new_instances = []
+for dzn_file in glob.glob(os.path.join(input_folder, "*.dzn")):
+    if not has_origin_train(dzn_file):
+        continue
+
+    out_file = os.path.join(output_folder, f"appear-{os.path.basename(dzn_file)}")
+    result = subprocess.run(["minizinc", generator_model, dzn_file], capture_output=True, text=True)
+
+    output_lines = result.stdout.rstrip("\n").split("\n")
+    truncated_output = "\n".join(output_lines[:-1]) + "\n"  # remove line with ---------- and add newline
+    with open(out_file, "w") as f:
+        f.write(truncated_output)
+
+    new_instances.append(os.path.basename(out_file))
+
+# create instances csv
+with open(instances_csv, "w", newline="") as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=["problem", "model", "data_file"])
+    writer.writeheader()
+
+    for instance in new_instances:
+        row = {
+            "problem": data_dir,
+            "model": "../models/solve_sat.mzn:../models/pddl-print.mzn",
+            "data_file": f"{data_dir}/{instance}",
+        }
+        writer.writerow(row)
